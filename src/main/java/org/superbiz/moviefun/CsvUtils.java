475399dcd531defdd1e95f84e8a81aba9f198063
simivalley <peopletruthbetold@gmail.com>
2019-05-07 16:23:38 -0700

    read csv files from war

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,8 +14,11 @@ import java.util.Scanner;
 public class CsvUtils {
 
     public static String readFile(String path) {
+
         try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
+            ClassLoader classLoader = CsvUtils.class.getClassLoader();
+            InputStream inputStream = classLoader.getResourceAsStream(path);
+            Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
 
             if (scanner.hasNext()) {
                 return scanner.next();
@@ -22,7 +26,7 @@ public class CsvUtils {
                 return "";
             }
 
-        } catch (FileNotFoundException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
