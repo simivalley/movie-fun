f08566069e8a7277f4e1f34a942e6ce4c07cd9f0
simivalley <peopletruthbetold@gmail.com>
2019-05-08 16:19:53 -0700

    updates

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -19,32 +19,32 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
-    @Bean
-    public ServletRegistrationBean actionServletRegistration(ActionServlet actionServlet) {
-        return new ServletRegistrationBean(actionServlet, "/moviefun/*");
-    }
-
-    @Bean
-    ServiceCredentials serviceCredentials(@Value("${vcap.services}") String vcapServices) {
-        return new ServiceCredentials(vcapServices);
-    }
-
-    @Bean
-    public BlobStore blobStore(
-            ServiceCredentials serviceCredentials,
-            @Value("${vcap.services.photo-storage.credentials.endpoint:#{null}}") String endpoint
-    ) {
-        String photoStorageAccessKeyId = serviceCredentials.getCredential("photo-storage", "user-provided", "access_key_id");
-        String photoStorageSecretKey = serviceCredentials.getCredential("photo-storage", "user-provided", "secret_access_key");
-        String photoStorageBucket = serviceCredentials.getCredential("photo-storage", "user-provided", "bucket");
-
-        AWSCredentials credentials = new BasicAWSCredentials(photoStorageAccessKeyId, photoStorageSecretKey);
-        AmazonS3Client s3Client = new AmazonS3Client(credentials);
-
-        if (endpoint != null) {
-            s3Client.setEndpoint(endpoint);
-        }
-
-        return new S3Store(s3Client, photoStorageBucket);
-    }
+//    @Bean
+//    public ServletRegistrationBean actionServletRegistration(ActionServlet actionServlet) {
+//        return new ServletRegistrationBean(actionServlet, "/moviefun/*");
+//    }
+//
+//    @Bean
+//    ServiceCredentials serviceCredentials(@Value("${vcap.services}") String vcapServices) {
+//        return new ServiceCredentials(vcapServices);
+//    }
+//
+//    @Bean
+//    public BlobStore blobStore(
+//            ServiceCredentials serviceCredentials,
+//            @Value("${vcap.services.photo-storage.credentials.endpoint:#{null}}") String endpoint
+//    ) {
+//        String photoStorageAccessKeyId = serviceCredentials.getCredential("photo-storage", "user-provided", "access_key_id");
+//        String photoStorageSecretKey = serviceCredentials.getCredential("photo-storage", "user-provided", "secret_access_key");
+//        String photoStorageBucket = serviceCredentials.getCredential("photo-storage", "user-provided", "bucket");
+//
+//        AWSCredentials credentials = new BasicAWSCredentials(photoStorageAccessKeyId, photoStorageSecretKey);
+//        AmazonS3Client s3Client = new AmazonS3Client(credentials);
+//
+//        if (endpoint != null) {
+//            s3Client.setEndpoint(endpoint);
+//        }
+//
+//        return new S3Store(s3Client, photoStorageBucket);
+//    }
 }
